60e42df64370409cf05de209ce3e7097e1f80c89
Luke Tomlinson <lukewtom93@gmail.com>
2025-01-26 12:29:42 +0000

    added PostListView tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='luke', password='123')
+
+    def test_can_list_posts(self):
+        luke = User.objects.get(username='luke')
+        Post.objects.create(owner=luke, title='title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='luke', password='123')
+        response = self.client.post('/posts/', {'title': 'title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_out_user_can_not_create_post(self):
+        response = self.client.post('/post/', {'title': 'title'})
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
