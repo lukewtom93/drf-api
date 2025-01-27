a1768e2b247b473bd9916ba5b2bacb98254135cf
Luke Tomlinson <lukewtom93@gmail.com>
2025-01-27 16:30:04 +0000

    refactored profile and post views

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -8,44 +8,50 @@ from drf_api.permissions import IsOwnerOrReadOnly
 
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
     List all profiles
     No Create view (post method), as profile creation handled by django signals
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Profile.objects.all()
+
+    # def get(self, request):
+    #     profiles = Profile.objects.all()
+    #     serializer = ProfileSerializer(
+    #         profiles, many=True, context={'request': request}
+    #     )
+    #     return Response(serializer.data)
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+
     
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+    # def get_object(self, pk):
+    #     try:
+    #         profile = Profile.objects.get(pk=pk)
+    #         self.check_object_permissions(self.request, profile)
+    #         return profile
+    #     except Profile.DoesNotExist:
+    #         raise Http404
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-        )
-        return Response(serializer.data)
+    # def get(self, request, pk):
+    #     profile = self.get_object(pk)
+    #     serializer = ProfileSerializer(
+    #         profile, context={'request': request}
+    #     )
+    #     return Response(serializer.data)
 
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self, request, pk):
+    #     profile = self.get_object(pk)
+    #     serializer = ProfileSerializer(
+    #         profile, data=request.data, context={'request': request}
+    #     )
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
