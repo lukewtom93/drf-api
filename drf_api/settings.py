ce9c03ebb7d0594d211b295e7350eff2a6780f2a
lukewtom93 <lukewtom93@gmail.com>
2025-02-09 16:35:28 +0000

    changed settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -115,11 +115,11 @@ MIDDLEWARE = [
 #          r"^https:\/\/.*\.codeinstitute-ide\.net$",
 #     ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-#     'https://moments-l-0479df3b77ea.herokuapp.com',
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://moments-l-0479df3b77ea.herokuapp.com',
 
-#     ]
+    ]
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
