cf289a093a8097b58182914a82a32f8de3f60c59
lukewtom93 <lukewtom93@gmail.com>
2025-02-09 16:06:46 +0000

    settings changed

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -121,16 +121,16 @@ CORS_ALLOWED_ORIGINS = [
 
     ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-    ]
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#          os.environ.get('CLIENT_ORIGIN')
+#     ]
 
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https:\/\/.*\.codeinstitute-ide\.net$",
-    ]
+# if 'CLIENT_ORIGIN_DEV' in os.environ:
+#     CORS_ALLOWED_ORIGIN_REGEXES = [
+#          r"^https:\/\/.*\.codeinstitute-ide\.net$",
+#     ]
 
 
 CORS_ALLOW_CREDENTIALS = True
