23d107c69033661f73745d714821ef9d4412266b
lukewtom93 <lukewtom93@gmail.com>
2025-02-10 17:37:57 +0000

    upgraded gj-rest-auth

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -27,14 +27,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [(
-    #     'rest_framework.authentication.SessionAuthentication'
-    #     if 'DEV' in os.environ
-    #     else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    # )],
-    'DEFAULT_AUTHENTICATION_CLASSES':
-       
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
